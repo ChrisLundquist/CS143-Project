53b3bac6cca2469a4b89596c097c85363ba15fe2
Dustin Lundquist <dustin@null-ptr.net>
2011-04-03 11:52:12 -0700

    Texture class

--- graphics/Texture.java
+++ graphics/Texture.java
@@ -1,5 +1,13 @@
 package graphics;
 
-public class Texture {
+import java.util.Vector;
 
+public class Texture {
+    private static Vector<Texture> textures = new Vector<Texture>();
+
+    private int glTexture;
+
+    public static Texture findById(int id) {
+        return textures.get(id);
+    }
 }
