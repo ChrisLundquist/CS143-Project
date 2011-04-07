4e0b959faa3715e8662808c5182f73b4ade1c62a
Dustin Lundquist <dustin@null-ptr.net>
2011-04-07 16:45:25 -0700

    More WavefrontObjLoader stuff

--- src/graphics/WavefrontObjLoader.java
+++ src/graphics/WavefrontObjLoader.java
@@ -8,6 +8,97 @@ import java.util.Vector;
  * Singleton class to load Wavefront .obj files
  */
 public class WavefrontObjLoader {
+    public static void main(String[] args) {
+        File file = new File("assets/cube.obj");
+        loadModel(file);
+    }
+    
+    public static Model loadModel(File file) {
+        Vector<ObjVertex> geo_verticies = new Vector<ObjVertex>();
+        Vector<ObjVertex> texture_verticies = new Vector<ObjVertex>();
+        Vector<ObjVertex> vertex_normals = new Vector<ObjVertex>();
+        Vector<Polygon> polygons = new Vector<Polygon>();
+        BufferedReader in;
+        String line;
+
+        try {
+            in = new BufferedReader(new FileReader(file));
+
+            while((line = in.readLine()) != null) {
+                StringTokenizer tokenizer = new StringTokenizer(line);
+
+                switch(tokenType(tokenizer.nextToken())) {
+                    case GEOMETRIC_VERTEX:
+                        geo_verticies.add(readVertex(tokenizer));
+                        break;
+                    case VERTEX_NORMAL:
+                        vertex_normals.add(readVertex(tokenizer));
+                        break;  
+                    case TEXTURE_COORDINATE:
+                        texture_verticies.add(readVertex(tokenizer));
+                        break;
+                    case FACE:
+                        polygons.add(readPolygon(tokenizer));
+                        
+                    default:
+                        System.out.println("Unhandled Line: " + line);
+                }
+            }
+
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }      
+    }
+
+    private static ObjVertex readVertex(StringTokenizer tokenizer) {
+        ObjVertex vertex = new ObjVertex();
+        float value;
+        
+        while (tokenizer.hasMoreTokens()) {
+            value = Float.parseFloat(tokenizer.nextToken());
+            vertex.add(value);
+        }
+        return vertex;
+    }
+    
+    private static Polygon readPolygon(StringTokenizer tokenizer) {
+        
+        while (tokenizer.hasMoreTokens()) {
+            String vertex_token = tokenizer.nextToken();
+            
+            
+            
+        }
+        
+        return null;
+    }
+
+    private static TokenType tokenType(String token) {
+        if (token.equals("#"))
+            return TokenType.COMMENT;
+        if (token.equals("v"))
+            return TokenType.GEOMETRIC_VERTEX;
+        if (token.equals("vt"))
+            return TokenType.TEXTURE_COORDINATE;
+        if (token.equals("vn"))
+            return TokenType.VERTEX_NORMAL;
+        if (token.equals("f"))
+            return TokenType.FACE;
+
+        return TokenType.UNKNOWN;
+    }
+
+    private static enum TokenType {
+        UNKNOWN,
+        COMMENT,
+        GEOMETRIC_VERTEX,
+        VERTEX_NORMAL,
+        TEXTURE_COORDINATE,
+        FACE,
+    }
+
     private static class ObjVertex {
         int dim;
         float x, y, z, w;
@@ -38,79 +129,5 @@ public class WavefrontObjLoader {
                     throw new IllegalArgumentException();
             }
         }
-    }
-    
-    public static Model loadModel(File file) {
-        Vector<ObjVertex> geo_verticies = new Vector<ObjVertex>();
-        Vector<ObjVertex> texture_verticies = new Vector<ObjVertex>();
-        Vector<ObjVertex> vertex_normals = new Vector<ObjVertex>();
-        BufferedReader in;
-        String line;
-
-        try {
-            in = new BufferedReader(new FileReader(file));
-
-            while((line = in.readLine()) != null) {
-                StringTokenizer tokenizer = new StringTokenizer(line);
-
-                switch(tokenType(tokenizer.nextToken())) {
-                    case GEOMETRIC_VERTEX:
-                        geo_verticies.add(readVertex(tokenizer));
-                        break;
-                    case VERTEX_NORMAL:
-                        vertex_normals.add(readVertex(tokenizer));
-                        break;  
-                    case TEXTURE_COORDINATE:
-                        texture_verticies.add(readVertex(tokenizer));
-                        break;
-                    case FACE:
-                        
-                    default:
-                        System.out.println("Unhandled Line: " + line);
-                }
-
-            }
-
-
-            return null;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }      
-    }
-
-    private static ObjVertex readVertex(StringTokenizer tokenizer) {
-        ObjVertex vertex = new ObjVertex();
-        float value;
-        
-        while (tokenizer.hasMoreTokens()) {
-            value = Float.parseFloat(tokenizer.nextToken());
-            vertex.add(value);
-        }
-        return vertex;
-    }
-
-    private static enum TokenType {
-        UNKNOWN,
-        COMMENT,
-        GEOMETRIC_VERTEX,
-        VERTEX_NORMAL,
-        TEXTURE_COORDINATE,
-        FACE,
-    }
-
-    private static TokenType tokenType(String token) {
-        if (token.equals("#"))
-            return TokenType.COMMENT;
-        if (token.equals("v"))
-            return TokenType.GEOMETRIC_VERTEX;
-        if (token.equals("vt"))
-            return TokenType.TEXTURE_COORDINATE;
-        if (token.equals("vn"))
-            return TokenType.VERTEX_NORMAL;
-        if (token.equals("f"))
-            return TokenType.FACE;
-
-        return TokenType.UNKNOWN;
     }
 }
