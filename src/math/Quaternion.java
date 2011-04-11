86cb2bc237b6d77706689019acb1593eb73e0879
Dustin Lundquist <dustin@null-ptr.net>
2011-04-11 16:33:24 -0700

    Fix quaternion constructor, wrote tests and toString() methods

--- src/math/Quaternion.java
+++ src/math/Quaternion.java
@@ -16,7 +16,7 @@ public class Quaternion {
         float radians = degrees / 180.0f * (float)Math.PI;
         // BOO java doubles
         float angle = (float)Math.sin(radians / 2.0f);
-        w_ = (float)Math.cos(angle / 2.0f);
+        w_ = (float)Math.cos(radians / 2.0f);
 
         x_ = x * angle;
         y_ = y * angle;
@@ -31,6 +31,27 @@ public class Quaternion {
         x_ = original.x_;
         y_ = original.y_;
         z_ = original.z_;
+    }
+
+    public boolean equals(Quaternion rhs) {
+        return (w_ == rhs.w_ && x_ == rhs.x_ && y_ == rhs.y_ && z_ == rhs.z_);
+    }
+
+    public String toString() {
+        return String.format("%g + %gi + %gj + %gk", w_, x_, y_, z_);
+    }
+
+    public String toMatrixString() {
+        float[] m = toGlMatrix();
+        return String.format(
+                "| %06.3f %06.3f %06.3f %06.3f |\n" +
+                "| %06.3f %06.3f %06.3f %06.3f |\n" +
+                "| %06.3f %06.3f %06.3f %06.3f |\n" +
+                "| %06.3f %06.3f %06.3f %06.3f |",
+                m[0], m[4], m[8], m[12],
+                m[1], m[5], m[9], m[13],
+                m[2], m[6], m[10], m[14],
+                m[3], m[7], m[11], m[15]);
     }
 
     public Quaternion times(Quaternion q) {
@@ -79,7 +100,24 @@ public class Quaternion {
     }
 
     public static void main(String[] args) {
-        Quaternion q = new Quaternion();
+        Quaternion negativeone = new Quaternion(); negativeone.w_ = -1.0f; negativeone.x_ = 0.0f; negativeone.y_ = 0.0f; negativeone.z_ = 0.0f;
+        Quaternion i = new Quaternion(); i.w_ = 0.0f; i.x_ = 1.0f; i.y_ = 0.0f; i.z_ = 0.0f;
+        Quaternion j = new Quaternion(); j.w_ = 0.0f; j.x_ = 0.0f; j.y_ = 1.0f; j.z_ = 0.0f;
+        Quaternion k = new Quaternion(); k.w_ = 0.0f; k.x_ = 0.0f; k.y_ = 0.0f; k.z_ = 1.0f;
+
+        // i^2 = j^2 = k^2 = ijk = -1
+        assert(negativeone.equals(negativeone));
+        assert(i.times(i).equals(negativeone));
+        assert(j.times(j).equals(negativeone));
+        assert(k.times(k).equals(negativeone));
+        assert(i.times(j).times(k).equals(negativeone));
+
+        // This transformation matrix should rotate 90 degrees about the x axis
+        assert(new Quaternion(1, 0, 0, 90).toMatrixString().equals(
+                "| 01.000 00.000 00.000 00.000 |\n" +
+                "| 00.000 00.000 01.000 00.000 |\n" +
+                "| 00.000 -1.000 00.000 00.000 |\n" +
+                "| 00.000 00.000 00.000 01.000 |"));
     }
 
     private float w_; // real
