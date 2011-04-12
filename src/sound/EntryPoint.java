461bc855c265d06692ceac68b4ceda8be256d8bf
Ty Overby <ty@pre-alpha>
2011-04-12 11:40:59 -0700

    Got everything working again

--- src/sound/EntryPoint.java
+++ src/sound/EntryPoint.java
@@ -2,13 +2,16 @@ package sound;
 import java.io.File;
 import java.util.ArrayList;
 
+import sound.Piece;
+import sound.Theme;
+
 public class EntryPoint {
     
     public static final String A_ASSETS = "assets/audio/";
 
     public static void main(String[] args) {
-        Piece p1 = new Piece(A_ASSETS+"200-0.5.wav");
-        Piece p2 = new Piece(A_ASSETS+"200-0.5.wav");
+        Piece p1 = new Piece(A_ASSETS+"200-0.5.wav","first");
+        Piece p2 = new Piece(A_ASSETS+"200-0.5.wav","second");
 
         ArrayList<Piece> pieces = new ArrayList<Piece>();
         pieces.add(p1);
--- src/sound/Piece.java
+++ src/sound/Piece.java
@@ -2,9 +2,6 @@ package sound;
 
 import java.io.File;
 
-<<<<<<< HEAD
-import javazoom.jl.player.advanced.AdvancedPlayer;
-
 /**
  * The Piece is just a part of a theme.  It contains one .wav file, and the name of that wav file.
  * The reason for wrapping this around the Sound class is that it will eventually have more differences, and I want
