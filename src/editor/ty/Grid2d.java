d4c9e721a3991b448b13187f7e276e678f4fbc79
Ty Overby <ty@pre-alpha.com>
2011-04-16 02:44:40 -0700

    made changes to how the 2d grid works as well as updating the testing window so that it correctly displays the three views as xy, xz, yz grids

--- src/editor/ty/Grid2d.java
+++ src/editor/ty/Grid2d.java
@@ -2,147 +2,188 @@ package editor.ty;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.SwingConstants;
 import javax.swing.JLabel;
 
+import actor.Actor;
+
 
 public class Grid2d extends JPanel {
-	private final Color VERTEX = Color.BLACK;
-	private final Color TEXT = Color.BLUE;
-	
-	private double scale = 1;
-	private JLabel posLbl;
+    private final Color VERTEX = Color.BLACK;
+    private final Color TEXT = Color.BLUE;
 
-	public Grid2d() {
-		addMouseMotionListener(new MouseMotionAdapter() {
-			public void mouseMoved(MouseEvent e) {
-				try{
-					mouseCoords(e.getX(),e.getY(),getGraphics());
-				}
-				catch(Exception e1){
-					e1.printStackTrace();
-				}
-			}
-		});		
-		this.setSize(500, 500);	
-		setLayout(null);
-		
-		JButton button = new JButton("+");
-		button.setBounds(12, 30, 44, 25);
-		button.setHorizontalAlignment(SwingConstants.LEFT);
-		add(button);
-		
-		JButton button_1 = new JButton("-");
-		button_1.setBounds(12, 67, 44, 25);
-		button_1.setHorizontalAlignment(SwingConstants.LEFT);
-		add(button_1);
-		
-		posLbl = new JLabel("(x,y)");
-		posLbl.setBounds(12, 3, 70, 15);
-		add(posLbl);
-	}
-	
-	public void paintComponent(Graphics g){
-		g.clearRect(0, 0, this.getWidth(), this.getHeight());
-		super.paintComponent(g);
-		this.drawAxis(g);
-		this.drawItems(g);
-	}
-	
-	private Point ltg(int x,int y){
-		int w = this.getWidth();
-		int h = this.getHeight();
-		
-		return new Point((int)((w/2+scale*x)),(int) ((h/2-scale*y)));
-	}
-	private Point ltg(Point p){
-		return ltg(p.x,p.y);
-	}
-	
-	private Point gtl(int x, int y){
-		int w = this.getWidth();
-		int h = this.getHeight();
-		
-		return new Point((int)(-(w/2-x)/scale),(int)((h/2-y)/scale));
-	}
-	private Point gtl(Point p){
-		return gtl(p.x,p.y);
-	}
-	
-	private void drawAxis(Graphics g){
-		int w = this.getWidth();
-		int h = this.getHeight();
-		
-		int hL = (int) (10/scale); //stands for hash Length
-		int spacing = (int) (10/scale);
-		
-		//draw the vertical axis
-		g.setColor(VERTEX);
-		g.drawLine(w/2, 0, w/2, h);
-		g.drawLine(0, h/2, w, h/2);
-		
-		Point center= ltg(20,20);
-		g.drawOval(center.x, center.y, 10, 10);
-		
-		//set hashes on horizontal
-		for(int i=0;i<(w/2)/scale;i+=spacing){
-			Point pos1 = ltg(i,-hL/2);
-			Point pos2 = ltg(i,hL/2);
-			
-			Point neg1 = ltg(-i,-hL/2);
-			Point neg2 = ltg(-i,hL/2);
-			
-			g.setColor(VERTEX);
-			g.drawLine(pos1.x, pos1.y, pos2.x, pos2.y);
-			g.drawLine(neg1.x, neg1.y, neg2.x, neg2.y);
-			
-		}
-		//vertical
-		for(int i=0;i<(h/2)/scale;i+=spacing){
-			Point pos1 = ltg(-hL/2,i);
-			Point pos2 = ltg(hL/2,i);
-			
-			Point neg1 = ltg(-hL/2,-i);
-			Point neg2 = ltg(hL/2,-i);
-			
-			g.drawLine(pos1.x, pos1.y, pos2.x, pos2.y);
-			g.drawLine(neg1.x, neg1.y, neg2.x, neg2.y);
-		}
-	}
-	
-	private void drawItems(Graphics g){
-		int defSize = 5;
-		int size = (int) (defSize * this.scale);
-		
-		
-	}
-	
-	public void mouseCoords(int x, int y,Graphics g){
-		g.clearRect(0, 0, this.getWidth(), this.getHeight());
-		//draw vertical line
-		g.drawLine(x, 0, x, this.getHeight());
-		//draw horizontal line
-		g.drawLine(0, y, this.getWidth(), y);
-		Point pos = gtl(x,y);
-		
-		g.drawString("("+pos.x+","+pos.y+")",10,10);
-		this.drawAxis(g);
-		this.drawItems(g);
-	}
-	
-	private static class Point{
-		public int x;
-		public int y;
-		
-		public Point(int x, int y){
-			this.x = x;
-			this.y = y;
-		}
-	}
+    private final Context context;
+    private ArrayList<Actor> actors = new ArrayList<Actor>();
+
+    private double scale = 1;
+
+    public Grid2d(Context context) {
+        addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseMoved(MouseEvent e) {
+                try{
+                    mouseCoords(e.getX(),e.getY());
+                }
+                catch(Exception e1){
+                    e1.printStackTrace();
+                }
+            }
+        });		
+        this.context = context;
+        this.setSize(500, 500);	
+        setLayout(null);
+    }
+
+    public void paintComponent(Graphics g){
+        g.clearRect(0, 0, this.getWidth(), this.getHeight());
+        super.paintComponent(g);
+        refresh();
+    }
+
+    private Point ltg(int x,int y){
+        int w = this.getWidth();
+        int h = this.getHeight();
+
+        return new Point((int)((w/2+scale*x)),(int) ((h/2-scale*y)));
+    }
+    private Point ltg(Point p){
+        return ltg(p.x,p.y);
+    }
+
+    private Point gtl(int x, int y){
+        int w = this.getWidth();
+        int h = this.getHeight();
+
+        return new Point((int)(-(w/2-x)/scale),(int)((h/2-y)/scale));
+    }
+    private Point gtl(Point p){
+        return gtl(p.x,p.y);
+    }
+
+    private void drawAxis(){
+        Graphics g = this.getGraphics();
+
+        int w = this.getWidth();
+        int h = this.getHeight();
+
+        int hL = (int) (10/scale); //stands for hash Length
+        int spacing = (int) (10/scale);
+
+        //draw the vertical axis
+        g.setColor(VERTEX);
+        g.drawLine(w/2, 0, w/2, h);
+        g.drawLine(0, h/2, w, h/2);
+
+        //set hashes on horizontal
+        for(int i=0;i<(w/2)/scale;i+=spacing){
+            Point pos1 = ltg(i,-hL/2);
+            Point pos2 = ltg(i,hL/2);
+
+            Point neg1 = ltg(-i,-hL/2);
+            Point neg2 = ltg(-i,hL/2);
+
+            g.setColor(VERTEX);
+            g.drawLine(pos1.x, pos1.y, pos2.x, pos2.y);
+            g.drawLine(neg1.x, neg1.y, neg2.x, neg2.y);
+
+        }
+        //vertical
+        for(int i=0;i<(h/2)/scale;i+=spacing){
+            Point pos1 = ltg(-hL/2,i);
+            Point pos2 = ltg(hL/2,i);
+
+            Point neg1 = ltg(-hL/2,-i);
+            Point neg2 = ltg(hL/2,-i);
+
+            g.drawLine(pos1.x, pos1.y, pos2.x, pos2.y);
+            g.drawLine(neg1.x, neg1.y, neg2.x, neg2.y);
+        }
+    }
+
+    private void drawActors(){
+        Graphics g = this.getGraphics();
+        for(Actor a: this.actors){
+            Point p;
+            int defSize = (int)a.getSize();
+            int size = (int) (defSize* this.scale);
+
+            switch(this.context){
+                case XY:
+                    p = new Point((int)a.getPosition().x,(int)a.getPosition().y);
+                    break;
+                case XZ:
+                    p = new Point((int)a.getPosition().x,(int)a.getPosition().z);
+                    break;
+                case YZ:
+                    p = new Point((int)a.getPosition().y,(int)a.getPosition().z);
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+
+            Point glo = ltg(p);
+            g.drawOval(glo.x-size/2, glo.y-size/2, size, size);
+        }
+    }
+    public void updateActors(ArrayList<Actor> actors){
+        this.actors = actors;
+        this.refresh();
+    }
+
+    private void mouseCoords(int x, int y){
+        Graphics g = this.getGraphics();
+
+        refresh();
+        //draw vertical line
+        g.drawLine(x, 0, x, this.getHeight());
+        //draw horizontal line
+        g.drawLine(0, y, this.getWidth(), y);
+        Point pos = gtl(x,y);
+
+        g.drawString("("+pos.x+","+pos.y+")",10,10);
+    }
+
+    public void zoom(double diff){
+        this.scale*=diff;
+        this.refresh();
+    }
+
+    public void refresh(){
+        try{
+            Graphics g = this.getGraphics();
+            g.clearRect(0, 0, this.getWidth(), this.getHeight());
+            this.drawAxis();
+            g.drawString(String.valueOf(this.scale), 10, 25);
+            this.drawActors();
+        }
+        catch(NullPointerException e){
+            //not ready yet.
+        }
+    }
+
+    private static class Point{
+        public int x;
+        public int y;
+
+        public Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    enum Context{
+        XY,
+        XZ,
+        YZ
+    }
 }
--- src/editor/ty/TestingWindow.java
+++ src/editor/ty/TestingWindow.java
@@ -2,8 +2,14 @@ package editor.ty;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
+
+import math.Vector3;
+
+import actor.Actor;
+import actor.PlayerShip;
 
 
 public class TestingWindow {
@@ -38,10 +44,34 @@ public class TestingWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setLayout(null);
 		
-		frame.add(new Grid2d(),BorderLayout.CENTER);
+		Grid2d xyGrid =  new Grid2d(Grid2d.Context.XY);
+		xyGrid.setBounds(0,0,300,300);
+		frame.add(xyGrid,BorderLayout.CENTER);
 		
-		frame.setBounds(300, 300, 450, 300);
+		Grid2d xzGrid =  new Grid2d(Grid2d.Context.XZ);
+        xzGrid.setBounds(300,0,300,300);
+        frame.add(xzGrid,BorderLayout.CENTER);
+        
+        Grid2d yzGrid =  new Grid2d(Grid2d.Context.YZ);
+        yzGrid.setBounds(600,0,300,300);
+        frame.add(yzGrid,BorderLayout.CENTER);
+        
+        ArrayList<Actor> actors = new ArrayList<Actor>();
+        
+        Actor ship = new PlayerShip();
+        ship.setPosition(new Vector3(40,0, -20));
+        ship.setSize(50);
+        actors.add(ship);
+        
+        xyGrid.updateActors(actors);
+        xzGrid.updateActors(actors);
+        yzGrid.updateActors(actors);
+        
+        
+		frame.setBounds(300, 300, 900, 300);
+		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
