fdb151cbc09b1873e5a165c76acb7f47c10b7e22
Chris Lundquist <rampantdurandal@gmail.com>
2011-04-29 13:25:05 -0700

    added vector3 tests, still more to do

--- test/test/math/Vector3Test.java
+++ test/test/math/Vector3Test.java
@@ -2,6 +2,8 @@ package test.math;
 
 import static org.junit.Assert.*;
 
+import math.Vector3;
+
 import org.junit.Test;
 
 public class Vector3Test {
@@ -28,12 +30,21 @@ public class Vector3Test {
 
     @Test
     public void testTimesFloat() {
-        fail("Not yet implemented"); // TODO
+        Vector3 v1 = new Vector3(0.0f, -2.0f, 6.3f);
+        assertEquals(new Vector3(0.0f, -4.0f, 12.6f),v1.times(2.0f));
+        assertEquals(v1,v1.times(1.0f));
     }
 
     @Test
     public void testPlus() {
-        fail("Not yet implemented"); // TODO
+        Vector3 v1 = new Vector3(1.0f,1.0f,1.0f);
+        Vector3 v1_copy = new Vector3(v1);
+        Vector3 v2 = new Vector3( -3.0f, 0.0f, 7.2f);
+        Vector3 v2_copy = new Vector3(v2);
+        assertEquals(new Vector3(-2.0f, 1.0f, -6.2f), v1.minus(v2));
+        assertEquals(v1_copy, v1);
+        assertEquals(v2_copy, v2);
+        
     }
 
     @Test
