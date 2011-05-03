251f86faf5c1c1491f2b9a51e94a064d9602b5b7
Tim <tim.mikeladze@gmail.com>
2011-05-03 12:37:30 -0700

    forgot to add

---
+++ src/actor/CapitalShip.java
@@ -0,0 +1,17 @@
+package actor;
+
+public class CapitalShip extends Actor {
+
+    public CapitalShip() {
+        super();
+    }
+    public void shoot() {
+        
+    }
+    @Override
+    public void handleCollision(Actor other) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
