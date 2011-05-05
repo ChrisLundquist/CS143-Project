495723e34235d6ef243b203c35848ad68f8f4c91
Dustin Lundquist <dustin@null-ptr.net>
2011-05-04 22:33:20 -0700

    Tests for ActorSet

---
+++ test/actor/ActorSetTest.java
@@ -0,0 +1,56 @@
+package actor;
+
+import static org.junit.Assert.*;
+import math.Vector3;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ActorSetTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+    @Test
+    public void testActorSet() {
+        ActorSet as = new ActorSet();
+    }
+    
+    @Test
+    public void testAdd() {
+        ActorSet as = new ActorSet();
+        as.add(new TestActor(Vector3.randomPosition(100)));
+    }
+
+    @Test
+    public void testIterator() {
+        ActorSet as = new ActorSet();
+        for (int i = 0; i < 100; i ++)
+            as.add(new TestActor(Vector3.randomPosition(100)));
+        
+        for (Actor a: as)
+            assertTrue(a instanceof TestActor);
+    }
+
+    @Test
+    public void testRemove() {
+        // Test that we can remove an actor while iterating over the set
+        ActorSet as = new ActorSet();
+        for (int i = 0; i < 100; i ++)
+            as.add(new TestActor(Vector3.randomPosition(100)));
+
+        int count = 0;
+        for (Actor a: as) {
+            if (count % 7 == 0)
+                as.remove(a);
+
+            count ++;
+        }
+    }
+}
---
+++ test/actor/TestActor.java
@@ -0,0 +1,14 @@
+package actor;
+
+public class TestActor extends Actor {
+    private static final long serialVersionUID = -4653206581455126209L;
+
+    public TestActor(math.Vector3 position) {
+        super();
+        this.position = position;
+    }
+
+    @Override
+    public void handleCollision(Actor other) {
+    }
+}
