24117da53a8678db24c278b2e66490465bca506e
Chris Lundquist <ChrisMLundquist@gmail.com>
2011-05-06 16:58:04 -0700

    corrected collision thread parameters, prevented infinite loops with concave geometry

--- src/physics/CollisionSolverThread.java
+++ src/physics/CollisionSolverThread.java
@@ -4,18 +4,19 @@ import actor.Actor;
 
 public class CollisionSolverThread extends Thread {
     java.util.List<Actor> actors;
-    protected int stride;
-    public CollisionSolverThread(java.util.List<Actor> actors,int stride){
-        if(stride <= 0)
+    protected final int stride, start;
+    public CollisionSolverThread(java.util.List<Actor> actors,int start, int stride){
+        if(stride <= 0 || start < 0)
             throw new IllegalArgumentException("CollisionSolverThread: Stride must be positive");
         this.actors = actors;
         this.stride = stride;
+        this.start = start;
     }
     
     
     private void checkCollisions(){
         // Check our guy we stride to against all the others
-        for(int i = stride; i < actors.size(); i += stride)
+        for(int i = start; i < actors.size(); i += stride)
             for(int j = i + 1; j < actors.size(); ++j){
                 Actor a = actors.get(i);
                 Actor b = actors.get(j);
