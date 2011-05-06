984b9025ad2ac99281add7b9f4293fa37ffb4de4
Dustin Lundquist <dustin@null-ptr.net>
2011-05-05 19:09:56 -0700

    Test all models

--- test/graphics/WavrfrontObjLoaderTest.java
+++ test/graphics/WavrfrontObjLoaderTest.java
@@ -1,28 +1,38 @@
 package graphics;
 
 import static org.junit.Assert.*;
+
+import java.io.File;
+
 import graphics.WavefrontObjLoader;
 
 import org.junit.Test;
 
 public class WavrfrontObjLoaderTest {
     public static final String MODEL_DIR = "assets/models/";
-    public static final String[] FILES = {
-        "cube_cube.obj",
-        "cessna.obj",
-        "shuttle.obj",
-        "bullet.obj",
-        "missile.obj",
-        "ship_test.obj"
-    };
+
 
     @Test
     /* No output should be printed about unhandled tokens */
     public void testLoadString() {
-        for(String file : FILES){
-            WavefrontObjLoader.load(MODEL_DIR + file);
+        File dir = new File(MODEL_DIR);
+        
+        // Loop through all models in models directory
+        for(String file: dir.list()) {
+            if (file.toLowerCase().endsWith(".obj"))
+                WavefrontObjLoader.load(MODEL_DIR + file);
+
+            if (WavefrontObjLoader.getErrors().size() > 0) {
+                for (WavefrontLoaderError err: WavefrontObjLoader.getErrors())
+                    System.err.println(err);
+                fail();
+            }
+
+            if (WavefrontMtlLoader.getErrors().size() > 0) {
+                for (WavefrontLoaderError err: WavefrontMtlLoader.getErrors())
+                    System.err.println(err);
+                fail();
+            }
         }
-        assertEquals(0, WavefrontMtlLoader.getErrors().size());
-        assertEquals(0, WavefrontObjLoader.getErrors().size());
     }
 }
