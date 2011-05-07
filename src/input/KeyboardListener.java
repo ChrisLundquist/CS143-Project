6415e134152fcd7ff7a644e3c36903c4bace5b45
Ty Overby <ty@pre-alpha.com>
2011-05-06 17:28:14 -0700

    made the keyboard not break with concurrency issues.  Thought I fixed this on my laptop, but I guess I didn't push

--- src/input/KeyboardListener.java
+++ src/input/KeyboardListener.java
@@ -1,6 +1,6 @@
 package input;
-import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -35,42 +35,46 @@ public class KeyboardListener implements KeyListener {
     }
 
     public void update() {
-        for(int key:currentKeys){
-            //Directions
-            if(key==Settings.Profile.Keys.forward){
-                InputRouter.sendAction(InputRouter.Interaction.FORWARD);
-            }
-            if(key==Settings.Profile.Keys.backward){
-                InputRouter.sendAction(InputRouter.Interaction.BACK);
-            }
+        synchronized(currentKeys) {
+            for(int key:currentKeys){
+                //Directions
+                if(key==Settings.Profile.Keys.forward){
+                    InputRouter.sendAction(InputRouter.Interaction.FORWARD);
+                }
+                if(key==Settings.Profile.Keys.backward){
+                    InputRouter.sendAction(InputRouter.Interaction.BACK);
+                }
 
-            //Pitch
-            if(key==Settings.Profile.Keys.pitchDown){
-                InputRouter.sendAction(InputRouter.Interaction.PITCH_DOWN);
-            }
-            if(key==Settings.Profile.Keys.pitchUp){
-                InputRouter.sendAction(InputRouter.Interaction.PITCH_UP);
-            }
+                //Pitch
+                if(key==Settings.Profile.Keys.pitchDown){
+                    InputRouter.sendAction(InputRouter.Interaction.PITCH_DOWN);
+                }
+                if(key==Settings.Profile.Keys.pitchUp){
+                    InputRouter.sendAction(InputRouter.Interaction.PITCH_UP);
+                }
 
-            //Roll
-            if(key==Settings.Profile.Keys.rollLeft){
-                InputRouter.sendAction(InputRouter.Interaction.ROLL_LEFT);
-            }
-            if(key==Settings.Profile.Keys.rollRight){
-                InputRouter.sendAction(InputRouter.Interaction.ROLL_RIGHT);
-            }
+                //Roll
+                if(key==Settings.Profile.Keys.rollLeft){
+                    InputRouter.sendAction(InputRouter.Interaction.ROLL_LEFT);
+                }
+                if(key==Settings.Profile.Keys.rollRight){
+                    InputRouter.sendAction(InputRouter.Interaction.ROLL_RIGHT);
+                }
 
-            //Yaw
-            if(key==Settings.Profile.Keys.yawLeft){
-                InputRouter.sendAction(InputRouter.Interaction.YAW_LEFT);
-            }
-            if(key==Settings.Profile.Keys.yawRight){
-                InputRouter.sendAction(InputRouter.Interaction.YAW_RIGHT);
-            }
+                //Yaw
+                if(key==Settings.Profile.Keys.yawLeft){
+                    InputRouter.sendAction(InputRouter.Interaction.YAW_LEFT);
+                }
+                if(key==Settings.Profile.Keys.yawRight){
+                    InputRouter.sendAction(InputRouter.Interaction.YAW_RIGHT);
+                }
 
-            //Shoot
-            if(key==Settings.Profile.Keys.shoot){
-                InputRouter.sendAction(InputRouter.Interaction.SHOOT);
+                //Shoot
+                if(key==Settings.Profile.Keys.shoot){
+                    InputRouter.sendAction(InputRouter.Interaction.SHOOT);
+                }
+
+                //System.out.println(key);
             }
         }
     }
