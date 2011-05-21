07bc779907224c3db53fa3c5841b48d45f6ea8bf
Chris Lundquist <ChrisMLundquist@gmail.com>
2011-05-20 20:17:57 -0700

    fixed out of index exception that broke game. cleaned up magic numbers. changed methods names to java convention

--- src/ship/PlayerShip.java
+++ src/ship/PlayerShip.java
@@ -15,98 +15,104 @@ public abstract class PlayerShip extends Ship {
     protected abstract float getAngularDampening();
     protected abstract float getVelocityDampening();
     protected abstract String getLocalModelName();
-    protected int[] ship_points = new int[4];
-    // power,shields,speed, availbale points, total points
-    // 0,1,2,3,4
+    protected int[] energyPoints = new int[4];
+    public static final byte POWER = 0;
+    public static final byte SHIELD = 1;
+    public static final byte SPEED = 2;
+    public static final byte TOTAL = 3;
+    
     public PlayerShip(){
         super();
         modelName = this.getLocalModelName();
-        //availibale points
-        ship_points[3]=12;
-        //total points
-        ship_points[4]=12;
+        energyPoints[TOTAL] = 12;
     }
-    public int getPower_points() {
-        return ship_points[0];
+    public int getPowerPoints() {
+        return energyPoints[POWER];
     }
-    public void setPower_points(int powerPoints) {
-        ship_points[0] = powerPoints;
+    public void setPowerPoints(int powerPoints) {
+        energyPoints[POWER] = powerPoints;
     }
     public void changePower_points(int change) {
 
     }
-    public int getShield_points() {
-        return ship_points[1];
+    public int getShieldPoints() {
+        return energyPoints[SHIELD];
     }
-    public void setShield_points(int shieldPoints) {
-        ship_points[1] = shieldPoints;
+    public void setShieldPoints(int shieldPoints) {
+        energyPoints[SHIELD] = shieldPoints;
     }
     /**
-     * Changes shield points and sets the shield strength while there are availble points availble 
+     * Changes shield points and sets the shield strength while there are available points available 
      * 
      * @param change the amount you want the shield points by
      */
-    public void changeShield_points(int change) {
-        if(getAvailible_points() > 0) {
-            ship_points[1] += change;
-            shields.get(0).setStrength(ship_points[1]*100);
+    public void changeShieldPoints(int change) {
+        if(getAvailablePoints() > 0) {
+            energyPoints[SHIELD] += change;
+            shields.get(0).setStrength(energyPoints[SHIELD]*100);
         }
     }
-    public int getSpeed_points() {
-        return ship_points[2];
+    public int getSpeedPoints() {
+        return energyPoints[SPEED];
     }
-    public void setSpeed_points(int speedPoints) {
-        ship_points[2] = speedPoints;
-    }
-
-    public int getTotal_points() {
-        return ship_points[4];
+    public void setSpeedPoints(int speedPoints) {
+        energyPoints[SPEED] = speedPoints;
     }
 
-    public void setTotal_points(int totalPoints) {
-        ship_points[4] = totalPoints;
+    public int getTotalPoints() {
+        return energyPoints[TOTAL];
     }
 
-    public int getAvailible_points() {
-        ship_points[3] = ship_points[4] - ( ship_points[0]+ ship_points[1] +  ship_points[2] );
-        return ship_points[3];
+    public void setTotalPoints(int totalPoints) {
+        energyPoints[TOTAL] = totalPoints;
     }
 
-    public void setAvailible_points(int availiblePoints) {
-        ship_points[3] = availiblePoints;
+    public int getAvailablePoints() {
+        return energyPoints[TOTAL] - energyPoints[POWER] - energyPoints[SHIELD] - energyPoints[SPEED];
     }
-    public String getAll_points() {
-        String s =  ship_points[0] + ","+  ship_points[1] + "," + ship_points[2] + "," +  ship_points[3] + "," +  ship_points[4];
+
+    public String getAllPoints() {
+        String s =  energyPoints[POWER] + ","+  energyPoints[SHIELD] + "," + energyPoints[SPEED] + "," +  energyPoints[TOTAL] + "," +  getAvailablePoints();
         return s;
     }
+    
     public void forwardThrust() {
-        velocity.plusEquals(getDirection().times(this.getAdditiveSpeed()));
+        velocity.plusEquals(getDirection().times(getAdditiveSpeed()));
     }
+    
     public void reverseThrust() {
-        velocity.minusEquals(getDirection().times(this.getNegativeSpeed()));
+        velocity.minusEquals(getDirection().times(getNegativeSpeed()));
     }
+    
     public void pitchUp(){
-        changePitch(this.getPitchRate());
+        changePitch(getPitchRate());
     }
+    
     public void pitchDown(){
-        changePitch(-this.getPitchRate());
+        changePitch(-getPitchRate());
     }
+    
     public void yawLeft() {
-        changeYaw(this.getYawRate());
+        changeYaw(getYawRate());
     }
+    
     public void yawRight() {
-        changeYaw(-this.getYawRate());
+        changeYaw(-getYawRate());
     }
+    
     public void rollLeft() {
-        changeRoll(this.getRollRate());
+        changeRoll(getRollRate());
     }
+    
     public void rollRight() {
-        changeRoll(-this.getRollRate());
+        changeRoll(-getRollRate());
     }
+    
     public void nextWeapon() {
         // Get the next weapon in the list
         setWeapon((selectedWeapon + 1) % weapons.size());
     }
+    
     public void previousWeapon() {
         setWeapon((selectedWeapon - 1) % weapons.size());
     }
@@ -119,7 +125,7 @@ public abstract class PlayerShip extends Ship {
     @Override
     public void update(){
         super.update();
-        dampenAngularVelocity(this.getAngularDampening());
-        velocity = velocity.times(this.getVelocityDampening());
+        dampenAngularVelocity(getAngularDampening());
+        velocity = velocity.times(getVelocityDampening());
     }
 }
