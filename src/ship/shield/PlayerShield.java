27e08a651d98e51af93888faab1ee6e91e30ae87
Ty Overby <ty@pre-alpha>
2011-05-24 13:21:43 -0700

    made the ship move forward by default, and cleaned up some Shields code

--- src/ship/shield/PlayerShield.java
+++ src/ship/shield/PlayerShield.java
@@ -1,6 +1,6 @@
 package ship.shield;
 
-public class PlayerShield extends ship.shield.Shield {
+public class PlayerShield extends Shield {
     private static final long serialVersionUID = -7318768199047822441L;
     final private int STRENGTH = 1000;
     
