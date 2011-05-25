8e60cc86384015c9f8aa20074f304f937b883d85
Chris Lundquist <rampantdurandal@gmail.com>
2011-05-24 23:10:42 -0700

    fixed statistics.sh to run with all finds. IN VIM

--- test/statistics.sh
+++ test/statistics.sh
@@ -1,6 +1,6 @@
-echo "typed up in nano, not vim"
+echo "typed up in nano, but fixed in vim"
 echo "we have this many lines of code" 
 git ls-files src | xargs cat | wc -l 
 echo "and this many classes"
 cd src
-find -type f | wc -l
+find . -type f | wc -l
