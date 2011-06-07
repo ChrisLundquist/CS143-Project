bef22df3fd0f9e6c34608ea73533135f148b3360
Ty Overby <ty@pre-alpha>
2011-06-07 14:04:40 -0700

    checks architecture

--- mainmenu.sh
+++ mainmenu.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 JAVA=/usr/lib/jvm/java-6-openjdk/bin/java
-JOGL_PATH=./jogamp-dist/jogl-2.0-b23-20110303-linux-amd64
-JOAL_PATH=./jogamp-dist/joal-2.0-20110302-linux-amd64
+
+if [ `uname -m` = "x86_64" ]; then
+    JOGL_PATH=./jogamp-dist/jogl-2.0-b23-20110303-linux-amd64
+    JOAL_PATH=./jogamp-dist/joal-2.0-20110302-linux-amd64
+else
+    JOGL_PATH=./jogamp-dist/jogl-2.0-b23-20110303-linux-i586
+    JOAL_PATH=./jogamp-dist/joal-2.0-20110302-linux-i586
+fi
 
 ${JAVA} \
     -Djava.library.path=${JOGL_PATH}/lib:${JOAL_PATH}/lib \
